5089781a2a4705081a35bcedb535a4f1d2e2772e
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-03-13 12:21:58 +0400

    inline svg

--- src/lib/socials.svelte
+++ src/lib/socials.svelte
@@ -1,43 +1,43 @@
 <script>
 	import Text from '../text.json';
 
-	import telegram from './assets/telegram.svg';
-	import twitter from './assets/twitter.svg';
-	import vk from './assets/vk.svg';
-	import instagram from './assets/instagram.svg';
-	import facebook from './assets/facebook.svg';
-	import youtube from './assets/youtube.svg';
+	import telegram from './assets/telegram.svg?raw';
+	import twitter from './assets/twitter.svg?raw';
+	import vk from './assets/vk.svg?raw';
+	import instagram from './assets/instagram.svg?raw';
+	import facebook from './assets/facebook.svg?raw';
+	import youtube from './assets/youtube.svg?raw';
 </script>
 
 <ul>
 	<li>
-		<a href="https:/t.me/spblpr" target="_blank">
-			<img alt={Text['Socials.Telegram']} src={telegram} width="42" height="42" />
+		<a href="https:/t.me/spblpr" target="_blank" aria-label={Text['Socials.Telegram']}>
+			{@html telegram}
 		</a>
 	</li>
 	<li>
-		<a href="https:/twitter.com/spblpr" target="_blank">
-			<img alt={Text['Socials.Twitter']} src={twitter} width="42" height="42" />
+		<a href="https:/twitter.com/spblpr" target="_blank" aria-label={Text['Socials.Twitter']}>
+			{@html twitter}
 		</a>
 	</li>
 	<li>
-		<a href="https:/vk.com/spblpr" target="_blank">
-			<img alt={Text['Socials.VK']} src={vk} width="42" height="42" />
+		<a href="https:/vk.com/spblpr" target="_blank" aria-label={Text['Socials.VK']}>
+			{@html vk}
 		</a>
 	</li>
 	<li>
-		<a href="https:/instagram.com/spblpr" target="_blank">
-			<img alt={Text['Socials.Instagram']} src={instagram} width="42" height="42" />
+		<a href="https:/instagram.com/spblpr" target="_blank" aria-label={Text['Socials.Instagram']}>
+			{@html instagram}
 		</a>
 	</li>
 	<li>
-		<a href="https:/facebook.com/spblpr" target="_blank">
-			<img alt={Text['Socials.Facebook']} src={facebook} width="42" height="42" />
+		<a href="https:/facebook.com/spblpr" target="_blank" aria-label={Text['Socials.Facebook']}>
+			{@html facebook}
 		</a>
 	</li>
 	<li>
-		<a href="https:/youtube.com/spblpr" target="_blank">
-			<img alt={Text['Socials.YouTube']} src={youtube} width="42" height="42" />
+		<a href="https:/youtube.com/spblpr" target="_blank" aria-label={Text['Socials.YouTube']}>
+			{@html youtube}
 		</a>
 	</li>
 </ul>
@@ -52,6 +52,8 @@
 	a {
 		display: block;
 		line-height: 0;
+		width: 42px;
+		height: 42px;
 
 		&:hover {
 			filter: brightness(120%);
