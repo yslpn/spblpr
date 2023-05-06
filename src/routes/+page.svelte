ff5830663f669a44ff4bdd49853b91077859abb0
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-05-07 02:04:08 +0400

    add og for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,12 @@
 	import Text from '../text.json';
 	import Link from '../lib/link.svelte';
 
+	import HeadMain from '$lib/headDonatePage.svelte';
+
 	import lprLogo from '../lib/assets/lpr-logo.svg?raw';
 </script>
+
+<HeadMain />
 
 <h1>{Text['MainPage.Title']}</h1>
 <p>{@html lprLogo}</p>
