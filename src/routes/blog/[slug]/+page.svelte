bbd9eb2f236df13a5f660068c9239e574f4cb55b
Yaroslav Slepukhin <y.i.slepukhin@ringcentral.com>
2024-04-19 13:22:46 +0400

    Add CSS styles for images in blog pages

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -7,3 +7,12 @@
 	<p>Опубликовано: {data.date}</p>
 	<svelte:component this={data.content} />
 </article>
+
+<style>
+	:global(img) {
+		display: block;
+		max-width: 100%;
+		height: auto;
+		margin: 0 auto;
+	}
+</style>
